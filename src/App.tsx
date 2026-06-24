4c509c788c8b006cdae36808ddcead80459b79d4
Damien Gasparina <dgasparina@cloudflare.com>
2026-06-24 11:37:19 +0200

    project initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,148 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import cloudflareLogo from './assets/cloudflare.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import JSZip from "jszip";
+
+interface DocImagePage {
+  image: string;
+  filename: string;
+  blob: Blob;
+}
+interface DocImages {
+  filename: string;
+  images: DocImagePage[];
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [name, setName] = useState('unknown')
+  const [isDragging, setIsDragging] = useState(false);
+  const [images, setImages] = useState<DocImages[]>([]);
+  const handleDragEnter = () => setIsDragging(true);
+  const handleDragLeave = () => setIsDragging(false);
+
+  const handleZipResponse = async (blob: Blob) => {
+    const zip = new JSZip();
+    const contents = await zip.loadAsync(blob);
+    const docImages: DocImages = {
+      filename: "doc",
+      images: []
+    }
+
+    for (const filename of Object.keys(contents.files)) {
+      const file = contents.files[filename];
+      const fileBlob = await file.async("blob");
+      const imageUrl = window.URL.createObjectURL(fileBlob);
+      const imagePage: DocImagePage = {
+        filename: filename,
+        image: imageUrl,
+        blob: fileBlob,
+      };
+      docImages.images.push(imagePage);
+    }
+    setImages([...images, docImages]);
+  };
+
+  const convertFilesToImages = (imageFiles: File[]) => {
+    for (const file of imageFiles) {
+      const data = new FormData();
+      data.append("file", file);
+      fetch("/api/doc2image", {
+        method: "POST",
+        body: data,
+      }).then(async (res) => {
+        if (!res.ok) {
+          console.error("Conversion failed", res);
+          alert("Docx conversion failed");
+          throw new Error("Upload failed");
+        }
+        const zipBlob = await res.blob();
+        handleZipResponse(zipBlob);
+      });
+    }
+  };
+
+  const handleDrop = (e: React.DragEvent) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const droppedFiles = Array.from(e.dataTransfer.files);
+    const imageFiles = droppedFiles.filter((file) =>
+      file.name.endsWith("docx"),
+    );
+    convertFilesToImages(imageFiles);
+  };
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started with Cloudflare</h1>
-          <p>
-            Edit <code>src/App.tsx</code> or <code>worker/index.ts</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none', padding: 0 }}>
-          <li>
-            <button
-              className="counter"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              Count is {count}
-            </button>
-          </li>
-          <li>
-          <button
-            className="counter"
-            onClick={() => {
-              fetch('/api/')
-                .then((res) => res.json())
-                .then((data) => setName(data.name))
-            }}
-            aria-label='get name'
-          >
-            Name from API is: {name}
-          </button>
-          </li>
-        </ul>
-
-
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-            <li>
-              <a href="https://workers.cloudflare.com/" target="_blank">
-                <img className="button-icon" src={cloudflareLogo} alt="" />
-                Workers Docs
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <div className="document">
+        <section
+          className={`document-upload ${isDragging ? "document-upload-dragging" : ""}`}
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+        >
+          <h1>
+            Drag & Drop <span className="code">docx</span> file here!
+          </h1>
+        </section>
+        {images.length > 0 && (
+          <section className="documents-render">
+            {images.map((image) => (
+              <section className="document-render" key={image.filename}>
+                <h1>{image.filename}</h1>
+                {image.images.map((page) => (
+                  <div key={page.filename}>
+                  <h2>{page.filename}</h2>
+                  <img src={page.image} />
+                  </div>
+                ))}
+              </section>
+            ))}
+          </section>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- worker/index.ts
+++ worker/index.ts
@@ -1,11 +1,16 @@
+import {env} from "cloudflare:workers";
+import { Doc2ImageContainer } from "./doc2image"
+
+export { Doc2ImageContainer }
+
 export default {
+
   fetch(request) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+    if (url.pathname.startsWith("/api/doc2image")) {
+      const container = env.Doc2Image.getByName(url.pathname);
+      return container.fetch(request);
     }
 		return new Response(null, { status: 404 });
   },
